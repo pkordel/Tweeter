0b9b022c452d2ffd2c9473d24152f69afb170bb8
Peter Kordel <pkordel@gmail.com>
2012-08-04 22:33:04 +0200

    Some cleanup

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,6 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
     @view_controller = TweetController.alloc.initWithNibName(nil, bundle: nil)
     @window.rootViewController = @view_controller
     @window.makeKeyAndVisible
--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -2,18 +2,17 @@ class TweetController < UIViewController
 
   def viewDidLoad
     super
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = self
-    @window.makeKeyAndVisible
+    window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    window.makeKeyAndVisible
 
-    @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
-    @button.setTitle("I finished the project!", forState:UIControlStateNormal)
-    @button.sizeToFit
-    @button.frame = CGRectMake(60, 88, 200, 37)
-    @window.addSubview(@button)
+    button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    button.setTitle("I finished the project!", forState:UIControlStateNormal)
+    button.sizeToFit
+    button.frame = CGRectMake(60, 88, 200, 37)
+    window.addSubview(button)
 
-    @button.addTarget(self, action:"button_tapped", 
-                      forControlEvents:UIControlEventTouchUpInside)
+    button.addTarget(self, action:"button_tapped", 
+                     forControlEvents:UIControlEventTouchUpInside)
   end
 
   def viewDidUnload
